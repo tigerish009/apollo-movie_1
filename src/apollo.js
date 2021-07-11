9e95f052e66e4b41390005fb4d804fbe050576a2
tigerish009 <tigerish009@gmail.com>
2021-07-11 17:42:47 +0900

    Local State

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,23 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: "https://movieql2.vercel.app/"
+  uri: "http://localhost:4000",
+  resolvers: {
+    Movie: {
+      isLiked: () => false
+    },
+    Mutation: {
+      likeMovie: (_, { id }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: true,
+            medium_cover_image: "lalalalal"
+          }
+        });
+      }
+    }
+  }
 });
 
 export default client;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,21 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
-  height: 380px;
+  height: 400px;
+  border-radius: 7px;
   width: 100%;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  overflow: hidden;
-  border-radius: 7px;
+  background-color: transparent;
 `;
 
 const Poster = styled.div`
@@ -17,12 +24,21 @@ const Poster = styled.div`
   width: 100%;
   background-size: cover;
   background-position: center center;
+  border-radius: 7px;
 `;
 
-export default ({ id, bg }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg} />
-    </Link>
-  </Container>
-);
+export default ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id) }
+  });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>
+        {isLiked ? "Unlike" : "Like"}
+      </button>
+    </Container>
+  );
+};
